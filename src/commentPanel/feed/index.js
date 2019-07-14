f5785e11041284036522211d6d586be58233d93d
Elena Y <alenkasmail@gmail.com>
2019-07-15 01:51:01 +0300

    comments v1

---
+++ src/commentPanel/feed/index.js
@@ -0,0 +1,95 @@
+import Component from './../lib/component';
+import Comment from './../comment';
+
+import template from './feed.mustache';
+import './feed.scss';
+
+
+export default class Feed extends Component {
+    get comments() {
+        return this._comments;
+    }
+
+    get $element() {
+        return this._$element;
+    }
+
+    constructor({
+                    posts: comments
+                }) {
+
+        super();
+
+        this._comments = [];
+
+        this._$element = this._createElement(template);
+
+        this.addComments(comments);
+
+    }
+
+    addComments(comments) {
+        if (!comments) return;
+
+        comments.forEach((comment) => this.addComment(comment));
+    }
+
+    addComment(post) {
+        const comment = new Comment(post);
+        this._comments.push(comment);
+
+        this._renderComment(comment);
+
+        comment.on('remove-comment', this._removeCommentHandler.bind(this));
+        comment.on('reply-comment', this._replyCommentHandler.bind(this));
+
+        return comment;
+    }
+
+    removeComment(id) {
+        const selector = "#" + id;
+        const $comment = this._$element.querySelector(selector);
+        console.log(this._$element);
+        console.log($comment);
+        this._comments = this._comments.filter((comment) => comment.comment.post.id !== id);
+        $comment.parentNode.removeChild($comment);
+    }
+
+    getAllComments() {
+        return this._comments.map((comment) => comment.commesnt);
+    }
+
+    _removeCommentHandler({
+                              detail: item
+                          }) {
+        const id = item.id;
+        console.log(id);
+        this.removeComment(id);
+        this._trigger('remove-comment', {
+            id
+        });
+    }
+
+    _replyCommentHandler({
+                             detail: item
+                         }) {
+        this._trigger('reply-comment', {
+            item
+        });
+    }
+
+    _renderComment(comment) {
+        let $parent = null;
+        if (comment.comment.post.hasOwnProperty("reid") && comment.comment.post.reid) {
+            $parent = this._$element.querySelector("#" + comment.comment.post.reid);
+        }
+        if ($parent) {
+            let $replyComment = $parent.querySelector(".reply");
+            $replyComment.appendChild(comment.$element);
+        } else {
+            this._$element.appendChild(comment.$element);
+        }
+    }
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import CommentPanel from './commentPanel';
+
+
+const app = new CommentPanel();
+document.body.appendChild(app.$element);
+
+
+/*
+menu.getElem().addEventListener('menu-select', function(event) {
+    alert(event.detail.value);
+});
+*/
